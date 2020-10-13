e77c2c474d3e11bbb6da76968ca837f7d78f6039
sid <sidharrth2002@gmail.com>
2020-10-14 00:45:12 +0800

    stopped hardsetting port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const http = require('http').Server(app);
 const ejs = require('ejs');
 
-let server = app.listen(3000, () => {
+let server = app.listen(process.env.PORT || 3000, () => {
     console.log("server is running on port", server.address().port);
 });
 
